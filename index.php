9cc8798a3d1e625190b22071d84ab90870b089a0
soikhukho <75216920+soikhukho@users.noreply.github.com>
2021-01-10 18:55:45 +0700

    2

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+	*{
+		margin: 0px;
+		padding: 0px;
+	}
+	li a
+	{
+		line-height: 30px;
+
+	}
+	ul li 
+	{
+      display: inline-block;
+      border-right: 1px solid black; 
+	}
+	ul li a
+	{
+		display: block;
+	}
+	ul li a: hover{
+		background: blue
+		color: white;
+		font-weight: bold;
+	}
+
+</style>
+<head>
+	<title>Vincent State School</title>
+	<meta charset="utf-8">
+</head>
+<body style="margin-left: 15%;margin-right: 15%;">
+<div id="header"; style="border-bottom: 1px solid green;height: 90px">
+	<div style="margin-left: 10%" align="center">
+		<img src="1.png" style="float: left;">
+	</div>
+	<div style="text-align: center; float: right; border-left: 1px solid black;width: 75%">
+		<h1 style="font-family: Arial, Helvetica, sans-serif; color:#6297BC">Vincent State School</h1>
+		<h2 style="color:#666666 ; font-weight: bold;font-family: tahoma; font-size: 12px  ">280 Palmerston, Townsill, 4814</h2>
+	</div>
+</div>
+<div id="menu" style="border bottom: 1px solid black; background: none ; " align="center">
+	<ul style="list-style-type: none;display:inline-block;">
+		<li style="display: inline-block;width: 90px;height:50px"><a href="home">Home</a></li>
+		<li style="display: inline-block;width: 90px;height:50px"><a href="information.html">Information Course</a></li>
+		<li style="display: inline-block;width: 90px;height:50px"><a href="Free.html">Free structure</a></li>
+		<li style="display: inline-block;width: 90px;height:50px"><a href="Admission.html">Admission Process</a></li>
+	</ul>
+</div>
+<div>
+	<b><h2>Welcome to Vincent State School</h2></b>
+	<b><h3>Welcome to Vincent State School</h3></b>
+	Our purpose in the community is to provide high quality learning opportunities for every student. We aim to promote and maintain a positive, safe and orderly learning environment for all students and staff
+    <h3><b>We plan to do this through:</b></h3>
+    <br>*The delivery of high quality curriculum</br>	
+    <br>*Excellence in teaching and learning</br>
+    <br>*Recognising diversity </br>
+	<br>* School/Home/Community Partnerships</br>	
+	<br>* Valuing Positive Behaviour</br>
+	This information guide is an introduction to what we hope will be a very happy association for you with Vincent State School.	
+</div>
+<ul>
+<li><a href="hhh.html">home.txt</a></li>
+<li><a href="information.html">home.txt</a></li>
+<li><a href="Free.html">home.txt</a></li>
+<li><a href="Admission">home.txt</a></li>
+</ul>
+
+</body>
+</html>
